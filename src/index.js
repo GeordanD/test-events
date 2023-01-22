0295a2213f2b6508c19becfcfbacd72e99a65a06
Geordan Daugherty <geordan_daugherty@hotmail.com>
2023-01-22 17:42:13 -0600

    yes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Container from './container';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<Container/>
 );
 
 // If you want to start measuring performance in your app, pass a function
